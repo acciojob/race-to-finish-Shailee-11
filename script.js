0a6f6a2589567ae456c2148ca454539760f0ad4d
Shailee-11 <128290114+Shailee-11@users.noreply.github.com>
2023-06-06 15:02:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 window.promises = [];
+var a =document.getElementsById("output");
 
-// Do not change the code above this
-// add your promises to the array `promises`
+
+function createPromise(){
+	return new promise((resolve, reject) => {
+		const random=getTime(1000,5000);
+		setTimeOut(()=>{
+			resolve(`${random}`);
+		},random)
+		
+	})
+}
+ const promises = Arrray.from({length:5}, createPromise);
+Promise.any(promises)
+.then((res)=>{
+	document.getElementsById("output").textContent=res;
+})
+.catch((error)=>{
+	console.log(error);
+})
+
+
+function getTime(a,b){
+	return Math.floor(Math.random()b-a+1)+a;
+}
+
